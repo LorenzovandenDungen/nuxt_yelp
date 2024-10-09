ecc7f2c9c0a538309986b2326535bb01b3b8d547
LorenzovandenDungen <git@stackblitz.com>
2024-10-09 20:30:34 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <Header />
+    <div class="flex flex-1 overflow-hidden">
+      <Sidebar />
+      <div class="flex flex-1 overflow-hidden">
+        <BusinessList />
+        <MapView />
+      </div>
+    </div>
+  </div>
+</template>
